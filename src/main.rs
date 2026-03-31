bcc5fb5671378c31d2a30799b2c7135852f6795c
Erik <erikgavs@gmail.com>
2026-03-31 12:11:02 +0200

    change

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn main() -> anyhow::Result<()> {
 
     let stream = device.build_input_stream(
         &config.into(),
-        |data: &[f32], _: &cpal::InputCallbackInfo| {
+        move |data: &[f32], _: &cpal::InputCallbackInfo| {
             let hight = data.iter().map(|s| s.abs()).fold(0.0f32, f32::max);
 
             if hight > 0.01 {
